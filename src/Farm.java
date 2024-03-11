93770dcf628a86d3cef45510d3751a2d5de4c62a
plichta <plichta@spsejecna.cz>
2024-03-11 09:09:15 +0100

    updated seed method

--- src/Farm.java
+++ src/Farm.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 public class Farm {
+    private int flowerArea;
+    private int aktualArea;
 private ArrayList <Animal> zviratka;
 private ArrayList <Flower> rostlinky;
 public void addAnimal(Animal animal){
@@ -10,10 +12,15 @@ public void removeAnimal(Animal animal){
     zviratka.remove(animal);
 }
 public void addRostlinka(Flower flower){
-    rostlinky.add(flower);
+    if (flower.getNeededArea()+aktualArea<=flowerArea) {
+        rostlinky.add(flower);
+    } else {
+        System.out.println("Not enough flower area ");
+    }
 }
 public void removeRostlinka(Flower flower){
     rostlinky.remove(flower);
 }
+
 
 }
